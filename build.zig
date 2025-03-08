aed6a03a635dfce21939ca236ac329b820dd601c
johan0A <arnaudchesse@outlook.com>
2025-03-08 16:48:18 +0100

    simplified zclay build.zig

--- build.zig
+++ build.zig
@@ -17,6 +17,7 @@ pub fn build(b: *B) void {
 
         const clay_dep = b.dependency("clay", .{});
         clay_lib.addIncludePath(clay_dep.path(""));
+
         clay_lib.addCSourceFile(.{
             .file = b.addWriteFiles().add("clay.c",
                 \\#define CLAY_IMPLEMENTATION
@@ -28,26 +29,15 @@ pub fn build(b: *B) void {
         break :blk clay_lib;
     };
 
-    {
-        const module = b.addModule("zclay", .{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-
-        module.linkLibrary(clay_lib);
-    }
+    const root_module = b.addModule("zclay", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    root_module.linkLibrary(clay_lib);
 
     {
-        const exe_unit_tests = b.addTest(.{
-            .root_module = b.createModule(.{
-                .root_source_file = b.path("src/root.zig"),
-                .target = target,
-                .optimize = optimize,
-            }),
-        });
-
-        exe_unit_tests.linkLibrary(clay_lib);
+        const exe_unit_tests = b.addTest(.{ .root_module = root_module });
 
         const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
         const test_step = b.step("test", "Run unit tests");
@@ -55,15 +45,7 @@ pub fn build(b: *B) void {
     }
 
     {
-        const tests_check = b.addTest(.{
-            .root_module = b.createModule(.{
-                .root_source_file = b.path("src/root.zig"),
-                .target = target,
-                .optimize = optimize,
-            }),
-        });
-
-        tests_check.linkLibrary(clay_lib);
+        const tests_check = b.addTest(.{ .root_module = root_module });
 
         const check = b.step("check", "Check if tests compile");
         check.dependOn(&tests_check.step);
