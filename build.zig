c439f254d974d4235be27157e530836e854d312c
johan0A <arnaudchesse@outlook.com>
2025-03-09 22:46:52 +0100

    updated raylib-zig dependency

--- build.zig
+++ build.zig
@@ -35,10 +35,8 @@ pub fn build(b: *B) void {
         .optimize = optimize,
     });
     root_module.linkLibrary(clay_lib);
-
     {
         const exe_unit_tests = b.addTest(.{ .root_module = root_module });
-
         const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
         const test_step = b.step("test", "Run unit tests");
         test_step.dependOn(&run_exe_unit_tests.step);
