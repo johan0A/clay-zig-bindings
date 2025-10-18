ae03b9cb276533eb0502845602eb09fe8ce2c393
zewenn <takaczsoli@gmail.com>
2025-10-18 11:10:56 +0200

    add: simple recursive testing

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+test {
+    std.testing.refAllDeclsRecursive(@This());
+}
+
 pub extern var CLAY_LAYOUT_DEFAULT: LayoutConfig;
 
 /// Color used for highlighting elements with the debug inspector panel (can be modified directly)
@@ -218,7 +222,9 @@ pub const Padding = extern struct {
     /// Padding on bottom side
     bottom: u16 = 0,
 
-    pub const xy = @compileError("renamed to axes"); // TODO: remove this in v0.3.0
+    /// # DEPRECATED
+    /// Use .axes instead.
+    pub const xy = axes; // TODO: remove this in v0.3.0
 
     /// Padding with vertical and horizontal values
     pub fn axes(top_bottom: u16, left_right: u16) Padding {
